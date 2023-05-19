76d5f3d2839f1fc378ee11bdb297a786cdeb021d
DennisMatenge <mateshdennis@gmail.com>
2023-05-19 22:16:26 +0300

    first commit

---
+++ src/components/Toggle.js
@@ -0,0 +1,16 @@
+import React, { useState} from "react";
+
+function Toggle() {
+  let [isOn, setIsOn] = useState(false)
+  function handleClick() {
+    setIsOn((isOn) = !isOn)
+  }
+  const color = isOn? "red" : "white";
+  return <>
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  </>
+}
+
+export default Toggle;
